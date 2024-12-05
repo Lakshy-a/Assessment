f1f05293d8799eff7efb96f5fc3243ce424dafdb
Lakshya Gupta <lakshyagupta8786@gmail.com>
2024-12-05 21:02:39 +0530

    new commit

--- src/app/loader/[loaderNumber]/page.jsx
+++ src/app/loader/[loaderNumber]/page.jsx
@@ -39,7 +39,7 @@ export default function Loader() {
           </div>
         </div>
         <div className="text-lg mt-4">
-          That's great! Just give me a moment to take notes
+          That s great! Just give me a moment to take notes
         </div>
         <button
           className="bg-white text-black px-4 py-2 rounded-lg"
